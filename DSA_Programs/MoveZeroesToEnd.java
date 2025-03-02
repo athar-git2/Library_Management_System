24db076b8771d8798a1f296b37056a45832e6408
athar-git2 <athar.connects@gmail.com>
2025-03-02 16:48:42 +0530

    Update MoveZeroesToEnd.java
    Added Using ArrayList

--- DSA_Programs/MoveZeroesToEnd.java
+++ DSA_Programs/MoveZeroesToEnd.java
@@ -61,42 +61,75 @@ public class MoveZeroesToEnd
 
 }
 
-// OUTPUT
+// OUTPUT******************
 
 // Enter no of elements: 7
 // Enter the array elements:  1 2 3 0 3 5 0
 // 1 2 3 3 5 0 0 
 
+//**********************Using ArrayList**************************
 
+public static void pushZerosAtEnd(ArrayList<Integer> arr)
+	{
+		int n = arr.size();
+
+		int j = -1;
+
+		for(int i = 0;i<n;i++)
+		{
+			if(arr.get(i) == 0)
+			{
+				j = i;
+				break;
+			}
+		}
+		
+		if(j == -1) {
+		return;
+		}
+		
+		for(int i = j+1; i<n; i++)
+		{
+			if(arr.get(i) != 0)
+			{
+				int temp = arr.get(i);
+				arr.set(i,arr.get(j));
+				arr.set(j,temp);
+				j++;
+			}
+		}
+	}
+
+//*****************************************************************
 //***********************BRUTE FORCE APPROACH**********************
 
-// import java.util.ArrayList;
+import java.util.ArrayList;
 
-// public class MoveZeroesToEnd {
-//         public static int[] moveZeros(int n, int []a) {
+public class MoveZeroesToEnd {
+        public static int[] moveZeros(int n, int []a) {
 
-//         ArrayList<Integer> temp = new ArrayList<>();
+        ArrayList<Integer> temp = new ArrayList<>();
 
-//         for(int  i = 0;i<n;i++)
-//         {
-//             if(a[i] !=0)
-//             {
-//                 temp.add(a[i]);
-//             }
-//         }
+        for(int  i = 0;i<n;i++)
+        {
+            if(a[i] !=0)
+            {
+                temp.add(a[i]);
+            }
+        }
 
-//         int j = temp.size();
+        int j = temp.size();
 
-//         for(int i=0;i<j;i++)
-//         {
-//            a[i] = temp.get(i);
-//         }
+        for(int i=0;i<j;i++)
+        {
+           a[i] = temp.get(i);
+        }
 
-//         for(int i =j ;i<n;i++)
-//         {
-//             a[i] = 0;
-//         }
-//         return a;
+        for(int i =j ;i<n;i++)
+        {
+            a[i] = 0;
+        }
+        return a;
 
-//     }
-// }
+    }
+}
