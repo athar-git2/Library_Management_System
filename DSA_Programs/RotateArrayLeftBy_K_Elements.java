51efc1afaa9cfe6a8be67e6294f7cea1eb79a2d3
athar-git2 <athar.connects@gmail.com>
2025-03-02 16:51:04 +0530

    Update RotateArrayLeftBy_K_Elements.java

--- DSA_Programs/RotateArrayLeftBy_K_Elements.java
+++ DSA_Programs/RotateArrayLeftBy_K_Elements.java
@@ -76,64 +76,61 @@ public class RotateArrayLeftBy_K_Elements {
 
 // ************************************************************
 
-// Optimised solution
+//Optimised solution
 
-// we can create another method reverse and solve this problem
+//we can create another method reverse and solve this problem
 
 
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class Prog {
+public class Prog {
     
-    
-//     public static void reverse(int arr[],int start,int end)
-//         {
-//             while(start <= end)
-//             {
-//                 int temp = arr[start];
-//                 arr[start] = arr[end];
-//                 arr[end] = temp;
-//                 start++;
-//                 end--;
-//             }
-//         }
+    public static void reverse(int arr[],int start,int end)
+        {
+            while(start <= end)
+            {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
 
-//     public static void RotateArray(int arr[],int k,int n)
-//     {
-//         reverse(arr, 0, k-1);
-//         reverse(arr, k, n-1);
-//         reverse(arr, 0, n-1);
+    public static void RotateArray(int arr[],int k,int n)
+    {
+        reverse(arr, 0, k-1);
+        reverse(arr, k, n-1);
+        reverse(arr, 0, n-1);
         
-//     }
-// 	public static void main (String[] args) throws java.lang.Exception
-// 	{
-// 		// your code goes here
-		
-// 		Scanner sc  = new Scanner(System.in);
-//         System.out.print("Enter Number of Elements: ");
-//         int n = sc.nextInt();
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{	
+		Scanner sc  = new Scanner(System.in);
+        System.out.print("Enter Number of Elements: ");
+        int n = sc.nextInt();
         
-//         System.out.print("Enter number of rotation: ");
-//         int k = sc.nextInt();
+        System.out.print("Enter number of rotation: ");
+        int k = sc.nextInt();
         
-//         System.out.print("Enter array elements: ");
+        System.out.print("Enter array elements: ");
         
-//         int arr [] = new int[n];
+        int arr [] = new int[n];
         
-//         for(int i = 0; i<n;i++)
-//         {
-//             arr[i] = sc.nextInt();
-//         }
+        for(int i = 0; i<n;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
         
-//         RotateArray(arr,k,n);
+        RotateArray(arr,k,n);
         
-//         for(int i = 0 ; i<n;i++)
-//         {
-//             System.out.print(arr[i] + " ");
-//         }
+        for(int i = 0 ; i<n;i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
 
-// 	}
-// }
+	}
+}
 
 
 // OUTPUT
