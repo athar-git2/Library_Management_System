10a95e2050e4c0ef23bd97c593928583bc1c32de
athar-git2 <athar.connects@gmail.com>
2025-04-24 23:00:43 +0530

    modified InsertionSort

--- Sorting_Algorithms/InsertionSort.java
+++ Sorting_Algorithms/InsertionSort.java
@@ -4,7 +4,7 @@ public class InsertionSort{
 
     public static void insertionSort(int arr[],int n)
     {
-        for(int i = 0; i <= n-1; i++)
+        for(int i = 1; i <= n-1; i++)
         {
             int j = i;
 
